29ebcb252635e5e810aad9e923e42087c91300c8
Sue <35784147+esesms@users.noreply.github.com>
2019-04-25 23:17:28 -0400

    Changed the window size to match the video aspect ratio 1. Added timings for Robert (every minute for seasonal veg?)

--- seasonal_vegetation/graph.py
+++ seasonal_vegetation/graph.py
@@ -11,10 +11,19 @@ with open('vegetation.json') as f:
 metric = 'percent live plant'
 
 ids = list(set([item['plot id'] for item in data]))
+#print(ids)
 ids = ['E3'] # n5 n6 n7 n10 e3 e9 e10
+#print(ids)
 signals = []
+signal = []
 for id in ids:
     signal = [item[metric] for item in data if item['plot id'] == id]
+    """for item in data:
+        if item['plot id'] == id:
+            print(item['plot id'])
+            signal.append(item[metric])
+            print(signal)
+    """
     ts = [item['t'] for item in data if item['plot id'] == id]
     # signal = normalize(signal, 0, 100)
     signal = normalize(signal)
@@ -26,9 +35,18 @@ for id in ids:
 # signal = np.mean(signals, axis=0)
 # print(signal)
 
-ctx = drawing.Context(1200, 500)
+ctx = drawing.Context(1600, 900)
 for s, signal in enumerate(signals):
-    ctx.plot(signal, stroke=colors[s], thickness=2.0)
+    ctx.plot(signal, stroke=colors[s], thickness=5.0)
+
+xMin = 0
+xMax = 1
+x = xMin
+reps = 10
+while x < xMax:
+    ctx.line(x, y1=0, x2=x, y2=1, stroke=colors[s], thickness=1.0, dash=None)
+    x += (xMax/reps)
+
 # ctx.plot(signal, stroke=colors[0])
 ctx.output('seasonal_vegetation.png')
 
